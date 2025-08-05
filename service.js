cb0bd18fd4769c0879a8de2e47d6779a55c7407e
Rafael Brandão <rafaelbrandaoreis@gmail.com>
2025-08-04 21:21:37 -0300

    fix: fix post of axios and add import

--- service.js
+++ service.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 async function enviarMensagemTexto(phone_number, texto) {
   await axios.post(
-    "https://graph.facebook.com/v18.0/YOUR_PHONE_NUMBER_ID/messages",
+    `https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages`,
     {
       messaging_product: "whatsapp",
       to: phone_number,
